a98bff8d84954a8049dd5444dfc2e8e6fe9bfac5
OmniaGomaa88 <minna1172018@gmail.com>
2025-06-26 23:35:06 +0200

    page home html

--- step-by-step-project/views/home.php
+++ step-by-step-project/views/home.php
@@ -1,1 +1,75 @@
-<p> home</p>
+<! Doctype html>
+<html lang="fr">
+    <head>
+        <meta charest ="UTF-8">
+        <title>Accueil</title>
+        <link rel="stylesheet" herf="style.css">
+    </head>
+    <body>
+        <p>hello</p>
+        <header>
+            <nav></nav>
+        </header>
+          <img src ="public\images\homePhoto.png" hight ="200px" width="200px">
+       <!--section de le paragraph prancipale de la site--> 
+        <section class="FlexCol" id="prancipal">
+          <h1> Vos objectifs en France <br> étape par étape. </h1>
+          <p> Une platforme pour guider les nouveaux arrivants <br> dans  leurs démarches éssentielles en france </p>
+          <button>Créer un compte</button>
+        </section> <!-- fin de section de le paragraph prancipale de la site--> 
+      <!--------------------------------------------------------------------------------->
+       <!-- section de les objectifs et title-->
+        <section class="FlexCol" id="objctifs">
+          <h2> Objectifs courants</h2>
+          <div> <!-- div contaner de les trois objectifs-->
+         <div class="FlexRow"><!-- div le logo et le title de lobjectif -->
+            <img src="" class="logo">
+            <p>Trouver un logement</p>  
+         </div><!-- fin div le logo et le title de lobjectif -->
+
+         <div class="FlexRow"><!-- div le logo et le title de lobjectif -->
+            <img src="" class="logo">
+            <p>Apprender le français</p>  
+         </div><!-- fin div le logo et le title de lobjectif -->
+
+         <div class="FlexRow"><!-- div le logo et le title de lobjectif -->
+            <img src="" class="logo">
+            <p>Obtenir un emploi</p>  
+         </div><!-- fin div le logo et le title de lobjectif -->
+        </div> <!-- fin div contaner de les trois objectifs-->
+        </section><!--fin section de les objectifs et title-->
+
+        <!--Section Comment ça marche ?-->
+        <section class="FlexCol" id="CaMarche">
+            <h2>Comment ça marche?</h2>
+            <article class="FlexRow">
+             <img>
+             <p>Choisissez votre objectif </p>
+            </article>
+
+             <article class="FlexRow">
+             <img>
+             <p>Suivez vos étapes</p>
+             </article>
+
+            <article class="FlexRow">
+             <img>
+             <p>Recervez des rappels</p>
+             </article>
+
+            <article class="FlexRow">
+             <img>
+             <p>Survelliez votre progrés</p>
+            </article>
+
+            <article class="FlexRow">
+             <img>
+             <p>Créer des nouveaux objectifs</p>
+            </article>
+
+        </section>
+        <!-- fin Section Comment ça marche ?-->
+
+
+    </body>
+</html>
